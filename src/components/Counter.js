a984e50ce7de7a2b18dbc942c5086b38886453a2
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-04-21 22:28:26 +0300

    lab attempted

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-
+const [count, setCount]= useState;
 function Counter() {
   const [count, setCount] = useState(0);
+
+  return <button>{count}</button>
 
   function increment() {
     setCount(count + 1);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <button onClick={increment}>{count}</button>;
 }
 
 export default Counter;
